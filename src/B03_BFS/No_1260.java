56c1f65470622a562e85a1521bc7b8de55e055fa
can <limduhwan@gmail.com>
2021-04-27 08:06:17 +0900

    04 27

---
+++ src/B03_BFS/No_1260.java
@@ -0,0 +1,14 @@
+package B03_BFS;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+//https://www.acmicpc.net/problem/1260
+//https://m.blog.naver.com/lm040466/221787478911
+public class No_1260 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("No_1260.txt"));
+    }
+
+}
